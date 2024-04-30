9262e1e95e76b9badc654f9adeca98270e4ba79b
pluginsAnalysis <168394492+pluginsAnalysis@users.noreply.github.com>
2024-04-30 18:07:07 +1000

    Update analysis_process.py

--- security and consistency analysis/analysis_process.py
+++ security and consistency analysis/analysis_process.py
@@ -1,6 +1,6 @@
 import analysis_method
 
-item = analysis_method.Json()
+item = analysis_method.Json(r"C:\Users\rrm_a\Desktop\test-2.xlsx")
 print("-----------step 1:try different url in the same domain ------------")
 item.write_down()
 item.inital_excel()
@@ -19,29 +19,32 @@ item.handle_list_2(0,'url5')
 item.save_changes()
 
 print('---------------------step 3:check api information and request------------------------------')
-item.get_api(0)
-item.save_changes()
-
+print("----task 1 get API ----")
+# item.get_api(0)
+# item.save_changes()
+print("----task 2 get API Information----")
 item.get_api_info(0)
 item.save_changes()
-
+print("----task 3 request----")
 item.clear_path(0)
 item.save_changes()
-
+print("----task 4 analysis result ----")
 item.request_result(0)
 item.save_changes()
 print('---------------------step 4:check request with token------------------------------')
+print("----task 1 analysis auth ----")
 item.check_auth(0)
 item.save_changes()
-
+print("----task 2 send request ----")
 item.detect_token(0)
 item.save_changes()
-
+print("----task 3  analysis result ----")
 item.request_result(0)
 item.save_changes()
 
 print("---------------------step 5:check name and legal----------------------------")
+print("----task 1 compare name ----")
 item.check_name(0)
 item.save_changes()
-
+print("----task 2 compare legal information ----")
 item.check_legal(0)
